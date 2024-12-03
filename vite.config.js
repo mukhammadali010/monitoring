26eae384e107239bf49d11dd6916f973a4508913
Muhammadali <test@Tests-MacBook-Pro.local>
2024-12-03 21:16:47 +0500

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path'
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path'
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        entryFileNames: `assets/[name].[hash].js`,
+      },
     },
+  },
+  server: {
+    host: true,
   },
 });
